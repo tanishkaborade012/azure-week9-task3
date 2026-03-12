a679a92a718f28e6e9d9b96c9308435b06e321fa
tanishkaborade012 <tanishkaborade012@gmail.com>
2026-03-12 19:42:01 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello from Tanushka's Azure .NET App 🚀");
+
+app.Run();
